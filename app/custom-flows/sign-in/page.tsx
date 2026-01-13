9ffe4cf40b9639cbba318de064907bf136ef1bc0
Jordan Bott <jbott@clerk.dev>
2026-01-13 11:37:25 -0800

    sign in/up w/ email otp init

--- app/custom-flows/sign-in/page.tsx
+++ app/custom-flows/sign-in/page.tsx
@@ -1,3 +1,129 @@
+"use client";
+
+import * as React from "react";
+import { useSignIn } from "@clerk/nextjs";
+import { PhoneCodeFactor, SignInFirstFactor } from "@clerk/types";
+import { useRouter } from "next/navigation";
+
 export default function CustomSignInPage() {
-  return <p>SignIn</p>;
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const [verifying, setVerifying] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [code, setCode] = React.useState("");
+  const router = useRouter();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!isLoaded && !signIn) return null;
+
+    try {
+      // Start the sign-in process using the phone number method
+      const { supportedFirstFactors } = await signIn.create({
+        identifier: email,
+      });
+
+      // Filter the returned array to find the 'phone_code' entry
+      const isEmailCodeFactor = (
+        factor: SignInFirstFactor
+      ): factor is EmailCodeFactor => {
+        return factor.strategy === "email_code";
+      };
+      const emailCodeFactor = supportedFirstFactors?.find(isEmailCodeFactor);
+
+      if (emailCodeFactor) {
+        // Grab the phoneNumberId
+        const { emailAddressId } = emailCodeFactor;
+
+        // Send the OTP code to the user
+        await signIn.prepareFirstFactor({
+          strategy: "email_code",
+          emailAddressId,
+        });
+
+        // Set verifying to true to display second form
+        // and capture the OTP code
+        setVerifying(true);
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+      // for more info on error handling
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  }
+
+  async function handleVerification(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!isLoaded && !signIn) return null;
+
+    try {
+      // Use the code provided by the user and attempt verification
+      const signInAttempt = await signIn.attemptFirstFactor({
+        strategy: "email_code",
+        code,
+      });
+
+      // If verification was completed, set the session to active
+      // and redirect the user
+      if (signInAttempt.status === "complete") {
+        await setActive({
+          session: signInAttempt.createdSessionId,
+          navigate: async ({ session }) => {
+            if (session?.currentTask) {
+              // Check for tasks and navigate to custom UI to help users resolve them
+              // See https://clerk.com/docs/guides/development/custom-flows/overview#session-tasks
+              console.log(session?.currentTask);
+              return;
+            }
+
+            router.push("/");
+          },
+        });
+      } else {
+        // If the status is not complete, check why. User may need to
+        // complete further steps.
+        console.error(signInAttempt);
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+      // for more info on error handling
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  }
+
+  if (verifying) {
+    return (
+      <>
+        <h1>Verify your email address</h1>
+        <form onSubmit={handleVerification}>
+          <label htmlFor="code">Enter your verification code</label>
+          <input
+            value={code}
+            id="code"
+            name="code"
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <button type="submit">Verify</button>
+        </form>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h1>Sign in</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Enter your email address</label>
+        <input
+          value={email}
+          id="email"
+          name="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Continue</button>
+      </form>
+    </>
+  );
 }
--- app/custom-flows/sign-up/page.tsx
+++ app/custom-flows/sign-up/page.tsx
@@ -1,3 +1,111 @@
-export default function CustomSignUpPage() {
-  return <p>SignUp</p>;
+"use client";
+
+import * as React from "react";
+import { useSignUp } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
+export default function CustomSignUp() {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [verifying, setVerifying] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [code, setCode] = React.useState("");
+  const router = useRouter();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!isLoaded && !signUp) return null;
+
+    try {
+      // Start the sign-up process using the phone number method
+      await signUp.create({
+        emailAddress: email,
+      });
+
+      // Start the verification - a SMS message will be sent to the
+      // number with a one-time code
+      await signUp.prepareEmailAddressVerification();
+
+      // Set verifying to true to display second form and capture the OTP code
+      setVerifying(true);
+    } catch (err) {
+      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+      // for more info on error handling
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  }
+
+  async function handleVerification(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!isLoaded && !signUp) return null;
+
+    try {
+      // Use the code provided by the user and attempt verification
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      // If verification was completed, set the session to active
+      // and redirect the user
+      if (signUpAttempt.status === "complete") {
+        await setActive({
+          session: signUpAttempt.createdSessionId,
+          navigate: async ({ session }) => {
+            if (session?.currentTask) {
+              // Check for tasks and navigate to custom UI to help users resolve them
+              // See https://clerk.com/docs/guides/development/custom-flows/overview#session-tasks
+              console.log(session?.currentTask);
+              return;
+            }
+
+            await router.push("/");
+          },
+        });
+      } else {
+        // If the status is not complete, check why. User may need to
+        // complete further steps.
+        console.error(signUpAttempt);
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+      // for more info on error handling
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  }
+
+  if (verifying) {
+    return (
+      <>
+        <h1>Verify your email address</h1>
+        <form onSubmit={handleVerification}>
+          <label htmlFor="code">Enter your verification code</label>
+          <input
+            value={code}
+            id="code"
+            name="code"
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <button type="submit">Verify</button>
+        </form>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Enter your email address</label>
+        <input
+          value={email}
+          id="email"
+          name="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Continue</button>
+      </form>
+    </>
+  );
 }
