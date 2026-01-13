542e0f5dde183a6ee4e1daf35956f69c603c9cb1
Jordan Bott <jbott@clerk.dev>
2026-01-13 12:37:30 -0800

    font theme adds

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Nexample",
@@ -26,9 +15,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-light-peach`}
-        >
+        <body className={`antialiased bg-light-peach font-mont`}>
           {children}
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="">
-      <main className=""></main>
+      <main className="text-4xl font-thin"></main>
     </div>
   );
 }
