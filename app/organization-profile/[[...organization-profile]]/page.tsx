ae76760562ee4c2271aef657fe2063f70ec09c3d
Jordan Bott <jbott@clerk.dev>
2026-01-16 09:14:07 -0800

    adds error handling for org profile page when org is not selected

--- app/organization-profile/[[...organization-profile]]/page.tsx
+++ app/organization-profile/[[...organization-profile]]/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import { OrganizationProfile } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 export default function OrganizationProfilePage() {
+  const auth = useAuth();
+
   return (
     <div className="flex w-screen h-[calc(100vh-3.75rem)] place-content-center items-center">
-      <OrganizationProfile />
+      {auth?.orgId ? (
+        <OrganizationProfile />
+      ) : (
+        <p className="text-dark-text text-xl">
+          You do not have an organization set, please select your organization
+          from the menu at the top left of the page!
+        </p>
+      )}
     </div>
   );
 }
